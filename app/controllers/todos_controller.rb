24838e6336136d4710b1e2d56805d562daf473da
David Albert <davidbalbert@gmail.com>
2013-04-27 21:20:40 -0400

    Ajax todo creation, completion, and destruction

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,11 +4,23 @@ class TodosController < ApplicationController
     @todo = @list.todos.build(todo_params)
 
     if @todo.save
-      flash[:success] = "Todo created!"
-      redirect_to @list
+      respond_to do |format|
+        format.html do
+          flash[:success] = "Todo created!"
+          redirect_to @list
+        end
+
+        format.js { @new_todo = @list.todos.build }
+      end
     else
-      flash.now[:error] = "There was an error creating your todo :("
-      render 'lists/show'
+      respond_to do |format|
+        format.html do
+          flash.now[:error] = "There was an error creating your todo :("
+          render 'lists/show'
+        end
+
+        format.js { @new_todo = @todo }
+      end
     end
   end
 
@@ -17,9 +29,15 @@ class TodosController < ApplicationController
     @todo = @list.todos.find(params[:id])
 
     if @todo.update_attributes(todo_params)
-      redirect_to @list
+      respond_to do |format|
+        format.html { redirect_to @list }
+        format.js
+      end
     else
-      render 'lists/show'
+      respond_to do |format|
+        format.html { render 'lists/show' }
+        format.js
+      end
     end
   end
 
@@ -28,7 +46,11 @@ class TodosController < ApplicationController
     @todo = @list.todos.find(params[:id])
 
     @todo.destroy
-    redirect_to @list
+
+    respond_to do |format|
+      format.html { redirect_to @list }
+      format.js
+    end
   end
 
   private
