0b34c8cc5b7dbf2bee9350bd617f6fba13f0e019
David Albert <davidbalbert@gmail.com>
2013-04-29 15:40:20 -0400

    Rails 4.0.0.rc1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Todos::Application.config.session_store :encrypted_cookie_store, key: '_todo_session'
+Todos::Application.config.session_store :cookie_store, key: '_todo_session'
